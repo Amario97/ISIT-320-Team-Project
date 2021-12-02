4d75fb4436287498f4507fd595241dbaac4e87ba
Amario97 <marioihab_ti2o@hotmail.com>
2021-12-01 22:14:39 -0800

    Adding UI pages

---
+++ pages/search.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+const Search = () => {
+  return (
+    <>
+      <Head>
+        <title>Meal Mediator | Search</title>
+        <meta name="keywords" content="mediator"/>
+      </Head>
+      <div>
+      <p align="center"><input type="text" placeholder="Search Restaurants"></input></p>
+        <h2>Recent Searches</h2>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      </div>
+    </>
+  );
+}
+ 
+export default Search;
