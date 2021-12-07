63dd3a08c84c87599d3785c90501c9e304bd2c87
Thai Anh Van Vu <cunhivu@gmail.com>
2021-12-06 17:55:13 -0800

    Added revisions to index.js and globals.css with new images on behalf of Dashia

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,26 @@ export default function Home() {
         <meta name="keywords" content="mediator"/>
       </Head>
 
-      <div>Add some homepage stuff here</div>
+      <h1>Welcome to Meal Mediator</h1>
+      <div>MealMediator gives users the ability to look up restaurants and choose where they would like to go. It will also allow users to choose multiple restaurants and have the app randomly choose one for them.</div>
+      <br></br>
+      <h2>Popular Dining Choices</h2>
+
+      <div class="row">
+        <div class="column">
+          <img src="/images/chinesefood.jpg" alt="Snow" width= {280} height= {200} />
+          <span class="caption">Chinese Food</span>
+        </div>
+        <div class="column">
+          <img src="/images/mexicanfood.jpg" alt="Forest" width= {280} height= {200} />
+          <span class="caption">Mexican Food</span>
+        </div>
+        <div class="column">
+          <img src="/images/indianfood.jpg" alt="Mountains" width= {280} height= {200} />
+          <span class="caption">Indian Food</span>
+        </div>
+      </div>
+
 
     </>
   )
