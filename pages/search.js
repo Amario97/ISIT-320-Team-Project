97d339f8cea10a53dd3b5537557be247d6c4016f
Thai Anh Van Vu <cunhivu@gmail.com>
2021-12-11 14:24:51 -0800

    Added Search randomizer on behalf of Samuel

--- pages/search.js
+++ pages/search.js
@@ -24,12 +24,18 @@ export async function fetcher(city) {
 }
 
 const Search = () => {
+  console.log("reloaded page *Inside*")
+  //A boolean for switching visibility of our api results
   var [divStyle, setDivStyle] = useState(false)
+  //MealMediator special choice
+  var [specialName, setSpecialName] = useState()
+  var [specialImage, setSpecialImage] = useState()
+  var [specialRating, setSpecialRating] = useState()
+  var [specialUrl, setSpecialUrl] = useState()
   
-  
-    //search bar input
+  //Search bar input
   var [input, setData] = useState("null")
-  //api data
+  //Setting the api data
   var [name, setName] = useState()
   var [imgUri, setImage] = useState()
   var [rating, setRating] = useState()
@@ -55,41 +61,281 @@ const Search = () => {
   var [rating6, setRating6] = useState()
   var [url6, setUrl6] = useState()
 
+  //items used to change index of business array to navigate page results
+  //predicting next navigation
+  var [item1, setItem1] = useState(0)
+  var [item2, setItem2] = useState(1)
+  var [item3, setItem3] = useState(2)
+  var [item4, setItem4] = useState(3)
+  var [item5, setItem5] = useState(4)
+  var [item6, setItem6] = useState(5)
+
+  //predicting previous navigation
+  var [item1b, setItem1b] = useState(0)
+  var [item2b, setItem2b] = useState(1)
+  var [item3b, setItem3b] = useState(2)
+  var [item4b, setItem4b] = useState(3)
+  var [item5b, setItem5b] = useState(4)
+  var [item6b, setItem6b] = useState(5)
+
+  //setting boolean variable according to whether previous/next button should be visible, according to result limit
+  var [nextVisible, setNextVisibility] = useState(false)
+  var [prevVisible, setPrevVisibility] = useState(false)
+
+  var [typed, setTyped] = useState(false)
+  //Gets called every time there is a change when we type in the search bar, saves each character.
   function inputChange(val) {
     setData(val.target.value)
+    setTyped(true)
     totalInput = input;
+
+    setItem1(0)
+    setItem2(1)
+    setItem3(2)
+    setItem4(3)
+    setItem5(4)
+    setItem6(5)
   }
 
-  async function submitHandler(event) {
+  function nextResults(event) {
     event.preventDefault();
+    scroll(0,0)
+    const nextSix = 6
+    console.log("Next RES Item 1: " + item1)
+    submitHandler(event, nextSix)
+  }
+
+  function previousResults(event) {
+    event.preventDefault();
+    scroll(0,0)
+    const prevSix = -6
+    console.log("Prev RES Item 1: " + item1)
+    submitHandlerB(event, prevSix)
+  }
+
+  //Method responding to the search button, gets the api data
+  async function submitHandler(event, plusSix = 6) {
+    event.preventDefault();
+    console.log("PLUS: " + plusSix + ", setItem1 = " + (item1 + plusSix))
+
+    //To set previous button invisible after pressing search button only after having typed something in search bar
+    if(typed) {
+      setPrevVisibility(false)
+      setTyped(false)
+    }
+  
+//predicting a next navigation
+    setItem1(item1 + plusSix)
+    setItem2(item2 + plusSix)
+    setItem3(item3 + plusSix)
+    setItem4(item4 + plusSix)
+    setItem5(item5 + plusSix)
+    setItem6(item6 + plusSix)
+//predicting a previous navigation
+    setItem1b(item1 - plusSix)
+    setItem2b(item2 - plusSix)
+    setItem3b(item3 - plusSix)
+    setItem4b(item4 - plusSix)
+    setItem5b(item5 - plusSix)
+    setItem6b(item6 - plusSix)
+
     totalInput = input;
     console.log("Input: " + input)
     fetcher(input).then(data => {
-      console.log("fetcher result : " +  data.businesses[0].name)
-      setName(data.businesses[0].name)
-      setImage(data.businesses[0].image_url)
-      setRating(data.businesses[0].rating)
-      setUrl(data.businesses[0].url)
-      setName2(data.businesses[1].name)
-      setImage2(data.businesses[1].image_url)
-      setRating2(data.businesses[1].rating)
-      setUrl2(data.businesses[1].url)
-      setName3(data.businesses[2].name)
-      setImage3(data.businesses[2].image_url)
-      setRating3(data.businesses[2].rating)
-      setUrl3(data.businesses[2].url)
-      setName4(data.businesses[3].name)
-      setImage4(data.businesses[3].image_url)
-      setRating4(data.businesses[3].rating)
-      setUrl4(data.businesses[3].url)
-      setName5(data.businesses[4].name)
-      setImage5(data.businesses[4].image_url)
-      setRating5(data.businesses[4].rating)
-      setUrl5(data.businesses[4].url)
-      setName6(data.businesses[5].name)
-      setImage6(data.businesses[5].image_url)
-      setRating6(data.businesses[5].rating)
-      setUrl6(data.businesses[5].url)
+      console.log("Fetcher for business 1 result : " +  data.businesses[0].name + ", Number of businesses: " + data.businesses.length)
+      if((item1 + 6) >= 12) {
+        setPrevVisibility(true)
+      }
+      if((item1 + 6 + 6) > data.businesses.length) {
+        setNextVisibility(false)
+      }
+      else {
+        setNextVisibility(true)
+      }
+      setName(data.businesses[item1].name)
+      setImage(data.businesses[item1].image_url)
+      setRating(data.businesses[item1].rating)
+      setUrl(data.businesses[item1].url)
+      setName2(data.businesses[item2].name)
+      setImage2(data.businesses[item2].image_url)
+      setRating2(data.businesses[item2].rating)
+      setUrl2(data.businesses[item2].url)
+      setName3(data.businesses[item3].name)
+      setImage3(data.businesses[item3].image_url)
+      setRating3(data.businesses[item3].rating)
+      setUrl3(data.businesses[item3].url)
+      setName4(data.businesses[item4].name)
+      setImage4(data.businesses[item4].image_url)
+      setRating4(data.businesses[item4].rating)
+      setUrl4(data.businesses[item4].url)
+      setName5(data.businesses[item5].name)
+      setImage5(data.businesses[item5].image_url)
+      setRating5(data.businesses[item5].rating)
+      setUrl5(data.businesses[item5].url)
+      setName6(data.businesses[item6].name)
+      setImage6(data.businesses[item6].image_url)
+      setRating6(data.businesses[item6].rating)
+      setUrl6(data.businesses[item6].url)
+
+      //Setting special MealMediator Choice
+      var randomNum = Math.floor( Math.random() * (6))
+      console.log("Random num: " + randomNum)
+      switch (randomNum) {
+        case 0:
+          setSpecialName(data.businesses[item1].name);
+          setSpecialImage(data.businesses[item1].image_url);
+          setSpecialRating(data.businesses[item1].rating);
+          setSpecialUrl(data.businesses[item1].url);
+          console.log("entered case: " + 0)
+          break;
+        case 1:
+          setSpecialName(data.businesses[item2].name);
+          setSpecialImage(data.businesses[item2].image_url);
+          setSpecialRating(data.businesses[item2].rating);
+          setSpecialUrl(data.businesses[item2].url);
+          console.log("entered case: " + 1)
+          break;
+        case 2:
+          setSpecialName(data.businesses[item3].name);
+          setSpecialImage(data.businesses[item3].image_url);
+          setSpecialRating(data.businesses[item3].rating);
+          setSpecialUrl(data.businesses[item3].url);
+          console.log("entered case: " + 2)
+          break;
+        case 3:
+          setSpecialName(data.businesses[item4].name);
+          setSpecialImage(data.businesses[item4].image_url);
+          setSpecialRating(data.businesses[item4].rating);
+          setSpecialUrl(data.businesses[item4].url);
+          console.log("entered case: " + 3)
+          break;
+        case 4:
+          setSpecialName(data.businesses[item5].name);
+          setSpecialImage(data.businesses[item5].image_url);
+          setSpecialRating(data.businesses[item5].rating);
+          setSpecialUrl(data.businesses[item5].url);
+          console.log("entered case: " + 4)
+          break;
+        case 5:
+          setSpecialName(data.businesses[item6].name);
+          setSpecialImage(data.businesses[item6].image_url);
+          setSpecialRating(data.businesses[item6].rating);
+          setSpecialUrl(data.businesses[item6].url);
+          console.log("entered case: " + 5)
+          break;
+        default:
+          console.log(`Nothin to choose`);
+      }
+      setDivStyle(true)
+    })
+  }
+
+  async function submitHandlerB(event) {
+    event.preventDefault();
+    const minusSix = -6
+    console.log("MINUS: " + minusSix + ", setItem1 = " + (item1 - 6))
+//predicting a previous navigation
+    setItem1b(item1b - 6)
+    setItem2b(item2b - 6)
+    setItem3b(item3b - 6)
+    setItem4b(item4b - 6)
+    setItem5b(item5b - 6)
+    setItem6b(item6b - 6)
+//predicting a next navigation
+    setItem1(item1b + 6)
+    setItem2(item2b + 6)
+    setItem3(item3b + 6)
+    setItem4(item4b + 6)
+    setItem5(item5b + 6)
+    setItem6(item6b + 6)
+
+    totalInput = input;
+    console.log("Input: " + input)
+    fetcher(input).then(data => {
+      console.log("Fetcher for business 1 result : " +  data.businesses[0].name + ", Number of businesses: " + data.businesses.length)
+      if((item1) <= data.businesses.length) {
+        setNextVisibility(true)
+      }
+      if((item1 - 6 - 6) <= 0) {
+        setPrevVisibility(false)
+      }
+      else {
+        setPrevVisibility(true)
+      }
+      setName(data.businesses[item1b].name)
+      setImage(data.businesses[item1b].image_url)
+      setRating(data.businesses[item1b].rating)
+      setUrl(data.businesses[item1b].url)
+      setName2(data.businesses[item2b].name)
+      setImage2(data.businesses[item2b].image_url)
+      setRating2(data.businesses[item2b].rating)
+      setUrl2(data.businesses[item2b].url)
+      setName3(data.businesses[item3b].name)
+      setImage3(data.businesses[item3b].image_url)
+      setRating3(data.businesses[item3b].rating)
+      setUrl3(data.businesses[item3b].url)
+      setName4(data.businesses[item4b].name)
+      setImage4(data.businesses[item4b].image_url)
+      setRating4(data.businesses[item4b].rating)
+      setUrl4(data.businesses[item4b].url)
+      setName5(data.businesses[item5b].name)
+      setImage5(data.businesses[item5b].image_url)
+      setRating5(data.businesses[item5b].rating)
+      setUrl5(data.businesses[item5b].url)
+      setName6(data.businesses[item6b].name)
+      setImage6(data.businesses[item6b].image_url)
+      setRating6(data.businesses[item6b].rating)
+      setUrl6(data.businesses[item6b].url)
+
+      //Setting special MealMediator Choice
+      var randomNum = Math.floor( Math.random() * (6))
+      console.log("Random num: " + randomNum)
+      switch (randomNum) {
+        case 0:
+          setSpecialName(data.businesses[item1b].name);
+          setSpecialImage(data.businesses[item1b].image_url);
+          setSpecialRating(data.businesses[item1b].rating);
+          setSpecialUrl(data.businesses[item1b].url);
+          console.log("entered case: " + 0)
+          break;
+        case 1:
+          setSpecialName(data.businesses[item2b].name);
+          setSpecialImage(data.businesses[item2b].image_url);
+          setSpecialRating(data.businesses[item2b].rating);
+          setSpecialUrl(data.businesses[item2b].url);
+          console.log("entered case: " + 1)
+          break;
+        case 2:
+          setSpecialName(data.businesses[item3b].name);
+          setSpecialImage(data.businesses[item3b].image_url);
+          setSpecialRating(data.businesses[item3b].rating);
+          setSpecialUrl(data.businesses[item3b].url);
+          console.log("entered case: " + 2)
+          break;
+        case 3:
+          setSpecialName(data.businesses[item4b].name);
+          setSpecialImage(data.businesses[item4b].image_url);
+          setSpecialRating(data.businesses[item4b].rating);
+          setSpecialUrl(data.businesses[item4b].url);
+          console.log("entered case: " + 3)
+          break;
+        case 4:
+          setSpecialName(data.businesses[item5b].name);
+          setSpecialImage(data.businesses[item5b].image_url);
+          setSpecialRating(data.businesses[item5b].rating);
+          setSpecialUrl(data.businesses[item5b].url);
+          console.log("entered case: " + 4)
+          break;
+        case 5:
+          setSpecialName(data.businesses[item6b].name);
+          setSpecialImage(data.businesses[item6b].image_url);
+          setSpecialRating(data.businesses[item6b].rating);
+          setSpecialUrl(data.businesses[item6b].url);
+          console.log("entered case: " + 5)
+          break;
+        default:
+          console.log(`Nothin to choose`);
+      }
       setDivStyle(true)
     })
   }
@@ -110,6 +356,18 @@ const Search = () => {
       </form>
       <div>
         <h2 align="center">Results</h2>
+
+        <div style={{visibility: divStyle ? 'visible' : 'hidden', align: 'center'}}>
+        <div className="gridMediator">
+              <a href={specialUrl} className="cardM">
+                <h3>{specialName}</h3>
+                <img src={specialImage} alt="Loading..." width="200" height="200"/>
+                <p align="center">Rating: {specialRating} ⭐</p>
+                <p align="center">🍽🌮 MealMediator's Pick! 🍱🍽</p>
+              </a>
+        </div>
+        </div>
+
         <div style={{visibility: divStyle ? 'visible' : 'hidden', align: 'center'}} className="gridMediator">
               <a href={url} className="cardM">
                 <h3>{name}</h3>
@@ -152,8 +410,12 @@ const Search = () => {
                 <img src={imgUri6} alt="Loading..." width="200" height="200"/>
                 <p align="center">Rating: {rating6} ⭐</p>
               </a>
-              <p align="center" id="enjoy">Enjoy!</p>
             </div>
+            <p align="center" style={{visibility: divStyle ? 'visible' : 'hidden'}}>Enjoy!</p>
+            <form align="right" style={{visibility: divStyle ? 'visible' : 'hidden'}} className="navForm">
+                <button style={{visibility: prevVisible ? 'visible' : 'hidden'}} onClick={previousResults} className="navButton1">Previous</button>
+                <button style={{visibility: nextVisible ? 'visible' : 'hidden'}} onClick={nextResults} className="navButton2">Next</button>
+            </form>
       </div>
       </main>
 
@@ -178,8 +440,29 @@ const Search = () => {
 
             .searchForm {
               position: relative;
+              clear: both;
             }
 
+            .navForm {
+              position: relative;
+              clear: both;
+            }
+
+            .navButton1 {
+              background-color: #7299fc;
+              border-color: #7299fc;
+              color: white;
+              border-radius: 55px;
+              right: 24%;
+            }
+
+            .navButton2 {
+              background-color: #7299fc;
+              border-color: #7299fc;
+              color: white;
+              border-radius: 55px;
+              position: absolute;
+            }
 
             .searchButton {
               background-color: #7299fc;
@@ -207,9 +490,7 @@ const Search = () => {
               align-items: center;
               justify-content: center;
               flex-wrap: wrap;
-              
               max-width: 1200px;
-              margin-top: 3rem;
             }
 
             .gridMediator img{
@@ -247,9 +528,6 @@ const Search = () => {
               line-height: 1.5;
             }
 
-            #enjoy {
-              clear: left;
-            }
     
     
             @media (max-width: 600px) {
